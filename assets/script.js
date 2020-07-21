293474137ac8c2836f4558ba0b1cdcf495d4b589
Dongwook-Lim <ldu9905@gmail.com>
2020-07-21 16:30:15 +0900

    apply buttonChange function

---
+++ assets/script.js
@@ -0,0 +1,59 @@
+function buttonChange1(self) {
+  if (self.textContent === "Sign up") {
+    self.style.backgroundColor = "#039be5";
+    self.style.color = "white";
+    self.style.borderColor = "#039be5";
+    self.textContent = "Done";
+    alert("Are you sure?");
+  } else {
+    self.style.backgroundColor = "white";
+    self.style.color = "#90caf9";
+    self.style.borderColor = "#90caf9";
+    self.textContent = "Sign up";
+  }
+}
+
+function buttonChange2(self) {
+  if (self.textContent === "Sign up for free") {
+    self.style.backgroundColor = "#039be5";
+    self.style.color = "white";
+    self.style.borderColor = "#039be5";
+    self.textContent = "Done";
+    alert("Are you sure?");
+  } else {
+    self.style.backgroundColor = "white";
+    self.style.color = "#90caf9";
+    self.style.borderColor = "#90caf9";
+    self.textContent = "Sign up for free";
+  }
+}
+
+function buttonChange3(self) {
+  if (self.textContent === "Get started") {
+    self.style.backgroundColor = "#039be5";
+    self.style.color = "white";
+    self.style.borderColor = "#039be5";
+    self.textContent = "Done";
+    alert("Are you sure?");
+  } else {
+    self.style.backgroundColor = "white";
+    self.style.color = "#90caf9";
+    self.style.borderColor = "#90caf9";
+    self.textContent = "Get started";
+  }
+}
+
+function buttonChange4(self) {
+  if (self.textContent === "Contact us") {
+    self.style.backgroundColor = "#039be5";
+    self.style.color = "white";
+    self.style.borderColor = "#039be5";
+    self.textContent = "Done";
+    alert("Are you sure?");
+  } else {
+    self.style.backgroundColor = "white";
+    self.style.color = "#90caf9";
+    self.style.borderColor = "#90caf9";
+    self.textContent = "Contact us";
+  }
+}
